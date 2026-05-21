dc0b8dd48e2d8209f5b254ca183f88e8fed08c72
Aurelie Nogueira <aurelienog@gmail.com>
2026-05-21 13:07:39 +0200

    added methods: get_cost() and get_distance()

--- src/domain/connection.py
+++ src/domain/connection.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+import math
 
 from typing import TYPE_CHECKING
 from dataclasses import dataclass
@@ -12,6 +13,7 @@ class Connection:
     hubs: tuple[Hub, Hub]
     max_link_capacity: int = 1
     occupation: int = 0
+    base_cost = None
 
     def get_neighbor(self, hub) -> Hub:
         if self.hubs[0] == hub:
@@ -26,3 +28,40 @@ class Connection:
 
     def remaining_capacity(self) -> int:
         return self.max_link_capacity - self.occupation
+
+    def get_distance(self, target: Hub) -> float:
+        # distance = √((x2-x1)² + (y2-y1)²)
+        source = self.get_neighbor(target)
+
+        dx = source.x - target.x
+        dy = source.y - target.y
+
+        return math.sqrt(dx*dx + dy*dy)
+
+    def get_cost(self, target: Hub):
+        """
+        Returns the intrinsic traversal cost of this edge.
+
+        This cost is static and independent from the simulation state.
+
+        Includes:
+            - geometric distance
+            - zone movement modifier
+            - structural penalties
+
+        Does NOT include:
+            - congestion
+            - reservations
+            - occupancy
+            - timestep-dependent costs
+        """
+
+        cost = self.get_distance(target)
+
+        if target.zone == "restricted":
+            cost += 5
+
+        elif target.zone == "priority":
+            cost -= 2
+
+        return max(1, cost)
