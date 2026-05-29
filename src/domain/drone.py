7288933c199312eca9b57d25d36077d28f885e9f
Aurelie Nogueira <aurelienog@gmail.com>
2026-05-29 11:09:32 +0200

    Drone instances are hashable

--- src/domain/drone.py
+++ src/domain/drone.py
@@ -4,6 +4,9 @@ from dataclasses import dataclass
 
 @dataclass
 class Drone:
-    id: int
+    id: str
     current_hub: Hub
     target_hub: Hub
+
+    def __hash__(self) -> int:
+        return hash(self.id)
