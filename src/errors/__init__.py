bfdf7c0da60fc88388c8d0c5edbeb5620403e283
Aurelie Nogueira <aurelienog@gmail.com>
2026-06-02 18:24:58 +0200

    added SimulationError

--- src/errors/__init__.py
+++ src/errors/__init__.py
@@ -1,6 +1,8 @@
 from .domain_errors import DomainError, HubError, ConnectionError
 from .parser_errors import ParseError, InvalidSyntaxError, SemanticError
+from .simulation_errors import SimulationError
 
 
 __all__ = ["DomainError", "HubError", "ConnectionError",
-           "ParseError", "InvalidSyntaxError", "SemanticError"]
+           "ParseError", "InvalidSyntaxError", "SemanticError",
+           "SimulationError"]
